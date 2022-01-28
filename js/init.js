ea13ca2d006a2d8c4e1f685efa97669fcd267b6d
Pedro Lucca Luciano <pedrolucca.luciano@gmail.com>
2022-01-28 19:15:55 -0300

    fist commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// Such info must be obtained from Firebase Project, on firebase console
+const firebaseConfig = {
+  apiKey: "AIzaSyAU0ULtAqBFOTn_WXw0B1Rfi2_1kwi5TSo",
+  authDomain: "todo-listt-6dd5e.firebaseapp.com",
+  projectId: "todo-listt-6dd5e",
+  storageBucket: "todo-listt-6dd5e.appspot.com",
+  messagingSenderId: "424657881669",
+  appId: "1:424657881669:web:8373a0e00d4e1a685a712c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
